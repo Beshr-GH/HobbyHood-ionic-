37ccd78d3dbc30e39d26a6466e3571d3ec9ec7ee
FirasHabri <firasalhabri@gmail.com>
2018-10-31 00:15:16 +0200

    Added logo files and applied logo on HOME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { ProfilePage } from '../pages/profile/profile';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = ProfilePage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
